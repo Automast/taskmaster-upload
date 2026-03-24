c0f636ede5ac029bdb82f20488f8f25cb7df05fb
Automast <54687228+Automast@users.noreply.github.com>
2026-03-24 18:16:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ const upload = multer({ storage: storage });
 
 app.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded' });
+        return res.status(400).json({ error: 'No file uploadedd' });
     }
     res.json({ message: 'File uploaded successfully', filename: req.file.filename });
 });
